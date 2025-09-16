071c1c95bf02a9f1340f61f30ce2f16b8b8144ca
marouane laghmich <marouan.laghmich00@gmail.com>
2025-09-16 17:49:21 +0100

    Update profiles.py

--- core/profiles.py
+++ core/profiles.py
@@ -44,4 +44,41 @@ class ProfileClass:
     def __init__(self, name):
         self.name = name
         self.encrypted_phrase = None  # Placeholder for encrypted validation phrase
-        # Add other profile-related attributes as needed
+        self.config_path = "./config.json"
+        self.database_path = "./database.db"
+        self.parameters = {
+            "company name": {"value": "", "display name":{"fr": "nom de l'entreprise", "es" : "nombre de la empresa"} ,"required": True, "default": "Lamibois", "options": ["Lamidap", "Lamibois", "porte amazone"], "type": "string"},
+            
+        }
+        self.available_parameters = {
+            "dialog" : ["company name"],
+            "table" : ["company name"]
+        }
+        
+    def get_value(self, param_key=None, distination=None):
+        if param_key:
+            return self.parameters.get(param_key, {}).get("value", None)
+        elif distination:
+            allowed_keys = self.available_parameters.get(distination, [])
+            return {key: self.parameters[key]["value"] for key in allowed_keys if key in self.parameters}
+        else:
+            return {key: param["value"] for key, param in self.parameters.items()}
+        
+    def set_value(self, param_key, value):
+        if param_key in self.parameters:
+            self.parameters[param_key]["value"] = value
+        else:
+            raise KeyError(f"Parameter '{param_key}' not found in product class.")
+        
+    def set_values(self, values_dict):
+        for key, value in values_dict.items():
+            if key in self.parameters:
+                self.parameters[key]["value"] = value
+            else:
+                raise KeyError(f"Parameter '{key}' not found in product class.")
+    
+    def load_config_data(self):
+        pass #TODO: implement loading data from database
+    
+    def save_to_config(self):
+        pass #TODO: implement saving data to database

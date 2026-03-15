737cbd7bd2251a13478bea0189f896dde95fd5ac
marouane laghmich <marouan.laghmich00@gmail.com>
2026-03-15 10:15:54 +0000

    basic updates

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ Application entry point - initializes and starts the PySide6 application
 """
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QGuiApplication, QIcon
 from ui.main_window import MainWindow
+
 
 def main():
     """Application entry point"""
     app = QApplication(sys.argv)
-    
+    app.setWindowIcon(QIcon("logo.png"))
+    QGuiApplication.styleHints().setColorScheme(Qt.ColorScheme.Dark)
+
     # Create and show main window
     window = MainWindow()
     window.show()

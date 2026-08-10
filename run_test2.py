083b19edea379360f2bd55deca595f171fc3c251
elbaqqalhaitam-wq <elbaqqalhaitam@gmail.com>
2026-08-10 02:18:28 +0100

    deleting the not importent files

--- run_test2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import sys
-import tests.test_recent_changes_full
-
-suite = unittest.TestSuite()
-suite.addTest(tests.test_recent_changes_full.TestBaseOperationDialogAsync("test_save_worker_success"))
-runner = unittest.TextTestRunner(stream=sys.stdout, verbosity=2)
-runner.run(suite)
